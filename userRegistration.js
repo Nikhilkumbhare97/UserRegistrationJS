e7f1091d89045c40d0b7defac9c6e3b65b4ad1c0
nikhilkumbhare <nikhilk190697@gmail.com>
2021-05-12 11:12:55 +0530

    Added: Valid First Name

--- userRegistration.js
+++ userRegistration.js
@@ -1,1 +1,12 @@
 console.log("Welcome To User Registration Problem");
+let userInput = require("readline-sync");
+
+//RegexPatterns
+const NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+
+let firstName = userInput.question("Enter your First Name : ");
+if (firstName.match(NAME_PATTERN)) {
+    console.log("Valid FirstName");
+} else {
+    console.log("Invalid FirstName");
+}
