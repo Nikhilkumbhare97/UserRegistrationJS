a1eddf7512453764db3bcdde44bbe654e2ab12ac
nikhilkumbhare <nikhilk190697@gmail.com>
2021-05-12 11:21:07 +0530

    Added: Valid Email

--- userRegistration.js
+++ userRegistration.js
@@ -3,6 +3,7 @@ let userInput = require("readline-sync");
 
 //RegexPatterns
 const NAME_PATTERN = "^[A-Z]{1}[a-z]{2,}$";
+const EMAIL_PATTERN = "^[A-Za-z0-9+-]+(\\.[A-Za-z0-9-]+)*@+[A-Za-z0-9-]+(\\.[A-Za-z0-9]{2,}){1,2}$";
 
 let firstName = userInput.question("Enter your First Name : ");
 if (firstName.match(NAME_PATTERN)) {
@@ -16,4 +17,11 @@ if (lastName.match(NAME_PATTERN)) {
     console.log("Valid LastName");
 } else {
     console.log("Invalid LastName");
+}
+
+let email = userInput.question("Enter your Email : ");
+if (email.match(EMAIL_PATTERN)) {
+    console.log("Valid Email");
+} else {
+    console.log("Invalid Email");
 }
